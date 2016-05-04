9dbc892b5eddb2ec617fcc77239ea46d485c5a4f
kkellyin90 <keith.kelly.a@gmail.com>
2016-05-04 12:25:00 +0530

    Added files via upload

---
+++ assignInteger.java
@@ -0,0 +1,13 @@
+
+public class assignInteger {
+
+	public static void main(String[] args) {
+		
+		//Declare three integer variables (a,b,c) 
+		//in a single declaration and simultaneously initialize b to 4
+		int a,b=4,c;
+		System.out.println("The value of b is "+b);
+
+	}
+
+}
